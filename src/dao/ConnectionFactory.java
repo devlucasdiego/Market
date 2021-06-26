18da607e826978681acf4d712e4be223d7c09f7f
Lucas Diego <luucasdiego_12@hotmail.com>
2021-06-26 15:49:39 -0400

    +Conexao com o BD mysql. +Inserir, Alterar, Excluir, Listar. + Exceptions

--- src/dao/ConnectionFactory.java
+++ src/dao/ConnectionFactory.java
@@ -1,5 +1,16 @@
 package dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ConnectionFactory {
 
+    public Connection getConexao() {
+        try {
+            return DriverManager.getConnection("jdbc:mysql://localhost/mercado", "root", "okabe1337");
+        } catch (SQLException erro) {
+            throw new RuntimeException("Erro 1: " + erro);
+        }
+    }
 }
